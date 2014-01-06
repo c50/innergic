58a91f900bc0f3e1b5579314d63acb38b6a88ca0
Eric Lemonne <eric@c50.be>
2014-01-06 01:01:47 +0100

    header improvements

--- header.php
+++ header.php
@@ -38,16 +38,22 @@
 			<img src="<?php header_image(); ?>" width="<?php echo get_custom_header()->width; ?>" height="<?php echo get_custom_header()->height; ?>" alt="">
 		</a>
 	</div>
-	<?php endif; ?>
+	<?php else : ?>
+	<div id="site-header">
+		<a href="<?php echo esc_url( home_url( '/' ) ); ?>" rel="home">
+			<img alt="" src="./images/cropped-spirale.jpg">
+		</a>
+	</div>
 
-	<header id="masthead" class="site-header" role="banner">
-		<div class="header-main">	
-			 
-			 <?php if ( get_theme_mod( 'innergic_logo' ) ) : ?>
+	<?php endif; ?>
+	<?php if ( get_theme_mod( 'innergic_logo' ) ) : ?>
                   <div class='site-logo'>
                      <a href='<?php echo esc_url( home_url( '/' ) ); ?>' title='<?php echo esc_attr( get_bloginfo( 'name', 'display' ) ); ?>' rel='home'><img src='<?php echo esc_url( get_theme_mod( 'innergic_logo' ) ); ?>' alt='<?php echo esc_attr( get_bloginfo( 'name', 'display' ) ); ?>'></a>
                   </div>
                 <?php endif; ?>
+
+	<header id="masthead" class="site-header" role="banner">
+		<div class="header-main">	
 
 			<h1 class="site-title"><a href="<?php echo esc_url( home_url( '/' ) ); ?>" rel="home"><?php bloginfo( 'name' ); ?></a></h1>
 			<div class="search-toggle">
